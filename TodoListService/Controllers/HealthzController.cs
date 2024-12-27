59bf77520e401bfab11baa7a3f993567d32f09c2
Anil Avvaru <anil.avvaru.cool@gmail.com>
2024-12-27 06:10:19 -0500

    b2c samples

---
+++ TodoListService/Controllers/HealthzController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthzController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Healthy";
+        }
+    }
+}
